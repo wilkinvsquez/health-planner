c32073cc428de7bf2777248803ac95ac9131002f
minorgm <magm1581@gmail.com>
2024-05-26 18:40:25 -0600

    Creation of utils.ts file with a method to verify if an object has empty fields, is being used to show the user a toast message in case the user info is incomplete

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,6 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 
+import { hasEmptyFields } from 'src/app/shared/services/utils';
+
+import { ToastService } from 'src/app/shared/services/toast.service';
+import { timeout } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,17 +17,42 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
 export class HomeComponent implements OnInit {
   user: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private _authService: AuthService,
+    private _toastService: ToastService
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.getCurrentUser();
+  }
 
   async getCurrentUser() {
-    await this.authService.getCurrentUser().then((res) => {
+    await this._authService.getCurrentUser().then((res) => {
       if (res) {
         this.user = res;
+        if (this.user) {
+          this.checkEmptyFields();
+        }
       } else {
         this.user = '';
       }
     });
+  }
+
+/**
+ * The function `checkEmptyFields` checks if any fields in the user object are empty and displays a
+ * warning message if so.
+ * @returns Nothing is being returned explicitly in the `checkEmptyFields` function. If the
+ * `hasEmptyFields` function returns true, a warning message will be shown using
+ * `_toastService.showWarning`. Otherwise, the function will return undefined.
+ */
+  checkEmptyFields() {
+    this.user.name = '';
+    if (hasEmptyFields(this.user)) {
+      this._toastService.showWarning(
+        'Completa la información de tu perfil');
+    } else {
+      return;
+    }
   }
 }
---
+++ src/app/shared/services/utils.ts
@@ -0,0 +1,26 @@
+/**
+ * The function `hasEmptyFields` checks if an object has any empty fields (empty strings, empty arrays,
+ * or nested objects with empty fields).
+ * @param {any} obj - The `obj` parameter in the `hasEmptyFields` function is an object of type `any`
+ * that contains key-value pairs. The function checks if any of the values in the object are empty
+ * strings, empty arrays, or nested objects with empty fields.
+ * @returns The function `hasEmptyFields` returns a boolean value - `true` if there are empty fields
+ * (empty strings, empty arrays, or nested objects with empty fields) in the input object `obj`, and
+ * `false` otherwise.
+ */
+export function hasEmptyFields(obj: any): boolean {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+
+            if (typeof value === 'string' && value.trim() === '') {
+                return true;
+            } else if (Array.isArray(value) && value.length === 0) {
+                return true;
+            } else if (typeof value === 'object' && hasEmptyFields(value)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
