f6dc619a1d8c07b53f5fa35c0ba846c76be1f6cf
wilkinvsquez <wilkinvsquez@gmail.com>
2024-05-27 19:51:54 -0600

    input error fix

--- src/app/shared/components/form/user-info-form/user-info-form.component.ts
+++ src/app/shared/components/form/user-info-form/user-info-form.component.ts
@@ -8,10 +8,11 @@ import {
 } from '@angular/forms';
 
 import { User } from 'src/app/core/interfaces/User';
-
-import { CustomInputComponent } from '../inputs/custom-input/custom-input.component';
 import { HomeComponent } from 'src/app/routes/home/home.component';
 import { ModalComponent } from 'src/app/shared/components/modal/modal.component';
+
+import { CustomInputComponent } from '../inputs/custom-input/custom-input.component';
+
 @Component({
   selector: 'app-user-info-form',
   templateUrl: './user-info-form.component.html',
@@ -20,11 +21,7 @@ import { ModalComponent } from 'src/app/shared/components/modal/modal.component'
     './user-info-form.component.scss',
   ],
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    CommonModule,
-    CustomInputComponent,
-  ],
+  imports: [ReactiveFormsModule, CommonModule, CustomInputComponent],
 })
 export class UserInfoFormComponent {
   @Output() userInfo = new EventEmitter<User>();
@@ -39,46 +36,39 @@ export class UserInfoFormComponent {
     this.userInfoForm = this._fb.group({
       identification: [
         this._homeComponent.user.identification || '',
-        [
-        Validators.required,
-        Validators.minLength(9),
-        Validators.maxLength(9)
-      ]],
-      name: ['', Validators.required],
-      lastname: [
-        this._homeComponent.user.lastname || '',
-        Validators.required
+        [Validators.required, Validators.minLength(9), Validators.maxLength(9)],
       ],
-      birthday: 
-      [
+      name: ['', Validators.required],
+      lastname: [this._homeComponent.user.lastname || '', Validators.required],
+      birthday: [
         this._homeComponent.user.birthday || '',
-      [
-        Validators.required,
-        Validators.pattern('^[0-9]{2}/[0-9]{2}/[0-9]{4}$')
-      ]],
-      email: [
-        this._homeComponent.user.email || '',
         [
           Validators.required,
-          Validators.email
-        ]],
+          Validators.pattern('^[0-9]{2}/[0-9]{2}/[0-9]{4}$'),
+        ],
+      ],
+      email: [
+        this._homeComponent.user.email || '',
+        [Validators.required, Validators.email],
+      ],
       phoneNumber: [
         this._homeComponent.user.phoneNumber || '',
-        Validators.required
+        Validators.required,
       ],
-      district: [
-        this._homeComponent.user.district || '',
-        Validators.required
-      ],
-      canton: [
-        this._homeComponent.user.canton || '',
-        Validators.required
-      ],
+      district: [this._homeComponent.user.district || '', Validators.required],
+      canton: [this._homeComponent.user.canton || '', Validators.required],
     });
   }
 
   isFieldInvalid(field?: any) {
     if (field === 'email') {
+      return (
+        this.userInfoForm.get(field)?.invalid &&
+        this.userInfoForm.get(field)?.pristine === false &&
+        this.userInfoForm.get(field)?.errors!['required']
+      );
+    }
+    if (field === 'identification') {
       return (
         this.userInfoForm.get(field)?.invalid &&
         this.userInfoForm.get(field)?.pristine === false &&
@@ -92,6 +82,8 @@ export class UserInfoFormComponent {
   }
 
   isFormatInvalid(field?: any) {
+    console.log(this.userInfoForm.get(field), field);
+
     if (field === 'email') {
       return (
         this.userInfoForm.get(field)?.errors &&
@@ -102,14 +94,27 @@ export class UserInfoFormComponent {
         this.userInfoForm.get(field)?.errors &&
         this.userInfoForm.get(field)?.errors!['minlength']
       );
+    } else if (field === 'identification') {
+      return (
+        this.userInfoForm.get(field)?.errors &&
+        this.userInfoForm.get(field)?.errors!['minlength']
+      );
     }
   }
 
   onSubmit() {
     this.isSubmitted = true;
     this._modalComponent.closeModal();
-    const { identification, name, lastname, birthday, email, phoneNumber, district, canton } = 
-    this.userInfoForm.value;
+    const {
+      identification,
+      name,
+      lastname,
+      birthday,
+      email,
+      phoneNumber,
+      district,
+      canton,
+    } = this.userInfoForm.value;
     this.userInfo.emit({
       identification,
       name,
@@ -118,7 +123,7 @@ export class UserInfoFormComponent {
       email,
       phoneNumber,
       district,
-      canton
+      canton,
     });
   }
 }
