cd6d73842832dde7a8c5501da7d89fecc828b90a
wilkinjsvsquez <wilkinvsquez@gmail.com>
2024-05-30 17:15:17 -0600

    User details page

---
+++ src/app/routes/users/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  standalone: true,
+})
+export class UserComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
