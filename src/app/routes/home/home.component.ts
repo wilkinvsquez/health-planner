2e8cc2c0fa5dc3dbadbcc788a38e1a8c7564c895
minorgm <magm1581@gmail.com>
2024-05-31 20:01:03 -0600

    minor fixes on the agenda component

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -14,22 +14,23 @@ import { hasEmptyFields } from 'src/app/shared/utils/utils';
   imports: [CommonModule, ModalComponent],
 })
 export class HomeComponent implements OnInit {
-  appointments = [
-    {patientName: "Minor", location: "Quesada, San Carlos", date: "2021-10-15", time: "10:00"},
-    {patientName: "Wilkin", location: "Quesada, San Carlos", date: "2021-10-15", time: "12:00"},
-    {patientName: "Jorge", location: "Quesada, San Carlos", date: "2021-10-15", time: "14:00"}
-  ];
-  
+  appointments: any[] = [];
+
   showModal: boolean = false;
   user: any;
 
   constructor(
     private _authService: AuthService,
     private _toastService: ToastService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getCurrentUser();
+    // this.appointments = [
+    //   { patientName: "Minor", location: "Quesada, San Carlos", date: "2021-10-15", time: "10:00" },
+    //   { patientName: "Wilkin", location: "Quesada, San Carlos", date: "2021-10-15", time: "12:00" },
+    //   { patientName: "Jorge", location: "Quesada, San Carlos", date: "2021-10-15", time: "14:00" }
+    // ];
   }
 
   async getCurrentUser() {
