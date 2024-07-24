8980e9a2002e90ff6a008aaa936d200d76c89532
minorgm <magm1581@gmail.com>
2024-07-23 23:46:11 -0600

    Disabling past days in calendar, and consuming holidays API to load holidays in the calendar

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -1,25 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
-import { CalendarModule, CalendarView } from 'angular-calendar';
+import { Component, EventEmitter, HostListener, OnInit, Output, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { CalendarModule, CalendarMonthViewDay, CalendarView } from 'angular-calendar';
 import { MonthViewDay, CalendarEvent } from 'calendar-utils';
 import { EventColor } from 'calendar-utils';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
+import { addMinutes, subDays } from 'date-fns';
+import { HttpClient } from '@angular/common/http';
+
+import { Holiday } from 'src/app/core/interfaces/Holiday';
 
 registerLocaleData(localeEs);
 
+type CalendarEventWithMeta = CalendarEvent<
+  { type: 'holiday'; holiday: Holiday } | { type: 'normal' }
+>;
+
+const COUNTRY_CODE = 'CR';
+
 import { addHours, intlFormat, set } from 'date-fns';
+import { Calendar } from 'primeng/calendar';
 const colors: Record<string, EventColor> = {
   blue: {
     primary: '#2c698d',
     secondary: '#D1E8FF',
   },
 };
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CalendarModule, CommonModule],
 })
 export class CalendarComponent implements OnInit {
@@ -31,6 +44,8 @@ export class CalendarComponent implements OnInit {
   view: CalendarView = CalendarView.Week;
   activeDayIsOpen: boolean = true;
   events: CalendarEvent[] = [];
+  // events: CalendarEventWithMeta[] = [];
+  holidays: CalendarEventWithMeta[] = [];
   eventHeight: number = 4;
   cellHeight: number = 4;
 
@@ -41,9 +56,10 @@ export class CalendarComponent implements OnInit {
     this.setCalendarView(event.target.innerWidth);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.fetchHolidays();
     this.setCalendarView(window.innerWidth);
     const start = set(new Date(), { year: 2024, month: 6, date: 22, hours: this.hour, minutes: 0, seconds: 0, milliseconds: 0 });
     this.calculateTop(start)
@@ -55,6 +71,43 @@ export class CalendarComponent implements OnInit {
       draggable: false,
       allDay: false,
     });
+  }
+
+  dateIsValid(date: Date): boolean {
+    return date > subDays(new Date(), 1);
+  }
+
+  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    body.forEach((day) => {
+      if (!this.dateIsValid(day.date)) {
+        day.cssClass = 'cal-disabled';
+      }
+    });
+  }
+
+  private fetchHolidays() {
+    this.http
+      .get<Holiday[]>(
+        `https://date.nager.at/api/v3/PublicHolidays/${new Date().getFullYear()}/${COUNTRY_CODE}`
+      )
+      .subscribe((holidays) => {
+        this.holidays = holidays.map((holiday) => {
+          const holidayDate = new Date(holiday.date);
+          const timezoneOffset = holidayDate.getTimezoneOffset();
+
+          return {
+            title: holiday.localName,
+            start: addMinutes(holidayDate, timezoneOffset),
+            allDay: true,
+            color: colors['blue'],
+            meta: {
+              type: 'holiday',
+              holiday,
+            },
+          };
+        });
+        this.events = [...this.events, ...this.holidays];
+      });
   }
 
   calculateTop(start: Date): void {
